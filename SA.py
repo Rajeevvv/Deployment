821797d90b7db9d8bec5557cc63edb5ac08e2cc5
K. Rajeev Gagan <109594589+Rajeevvv@users.noreply.github.com>
2023-03-09 18:50:02 +0530

    Update SA.py

--- SA.py
+++ SA.py
@@ -8,6 +8,8 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk 
+nltk.download('stopwords')
+nltk.download('punkt')
 
 input_text = st.text_input("Enter text for Sentimental Analysis:")
 
